3aa9f616f7d38470572dfc54ea1c1874420971f0
zeynepayar <zeynepayar2949@gmail.com>
2024-04-24 14:53:03 +0300

    change

--- app/Views/stock.php
+++ app/Views/stock.php
@@ -1,27 +1,27 @@
-<?php include ("header.php"); ?>
-    <div class="table">
-        <table class="table  table-hover" style="background:#f2f2f2;">
-            <thead>
-                <tr>
-                    <th scope="col">İlaç ID</th>
-                    <th scope="col">Adet</th>
-                    <th scope="col">Kategori</th>
-                    <th>Kullanım Süresi</th>
-                    <th scope="col">İlaç Sayısı</th>
-                    <th scope="col">Stok Görüntüle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Kırmızı </td>
-                    <td>08.04.2024</td>
-                    <td>1 Gün</td>
-                    <td>1</td>
-                    <td><button class="btn btn-sm" style="color: #808080; background:#ffa500; font-weight: bold;">GÖRÜNTÜLE</button></td>
-                </tr>
-            </tbody>
-        </table>
-    </div> 
-</body> 
+<?php include("header.php"); ?>
+<div class="table">
+    <table class="table  table-hover" style="background:#f2f2f2;">
+        <thead>
+            <tr>
+                <th scope="col">İlaç ID</th>
+                <th scope="col">İlaç Adı</th>
+                <th scope="col">Stok</th>
+                <th scope="col">Kategori</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>PAROL </td>
+                <td> 10 kutu</td>
+                <td>hap</td>
+
+
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+
 </html>
