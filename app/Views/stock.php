9fa7a1c3b7bebe0113df4fd16dacb186ffb02b8d
Sude Suvar <sudesuvar51@gmail.com>
2024-04-29 20:15:59 +0300

    update stock

--- app/Views/stock.php
+++ app/Views/stock.php
@@ -1,65 +1,79 @@
 <?php include("header.php"); ?>
 <div class="table">
-    <table class="table  table-hover" style="background:#f2f2f2;">
-        <thead>
-            <tr>
-                <th scope="col">Güncelle</th>
-                <th scope="col">Stok İD</th>
-                <th scope="col">İlaç Adı</th>
-                <th scope="col">Stok Adedi</th>
-                <th scope="col">Kategori</th>
+  <table class="table  table-hover" style="background:#f2f2f2;">
+    <thead>
+      <tr>
+        <th scope="col">Güncelle</th>
+        <th scope="col">Stok İD</th>
+        <th scope="col">İlaç Adı</th>
+        <th scope="col">Stok Adedi</th>
+        <th scope="col">Kategori</th>
 
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($stock as $stocks) : ?>
-                <tr id="tableID_<?php echo $stocks->stock_id; ?>">
-                    <td>
-                        <button class="btn btn-sm" type="button" id="delete_<?php echo $stocks->stock_id; ?>" style="color: #808080; background:#ffa500; font-weight: bold;">
-                            <i class="fa-solid fa-trash"></i>&nbsp;Sil
-                        </button>
-                        &nbsp;
-                        <button class="btn btn-sm" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?php echo $stocks->stock_id; ?>" style="color: #808080; background:#ffa500; font-weight: bold;">
-                            <i class="fa-solid fa-pen-to-square"></i>&nbsp;Düzenle
-                        </button>
-                    </td>
-                    <td id="tableStock_id"><?php echo $stocks->stock_id; ?></td>
-                    <td id="tableStock_med"><?php echo $stocks->medicine_id; ?></td>
-                    <td id="tableStock_piece"><?php echo $stocks->piece; ?></td>
-                    <td id="tableStock_piece"><?php echo $stocks->category_id; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($stock as $stocks) : ?>
+        <tr id="tableID_<?php echo $stocks->stock_id; ?>">
+          <td>
+            <button class="btn btn-sm" type="button" id="delete_<?php echo $stocks->stock_id; ?>" style="color: #808080; background:#ffa500; font-weight: bold;">
+              <i class="fa-solid fa-trash"></i>&nbsp;Sil
+            </button>
+            &nbsp;
+            <button class="btn btn-sm" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?php echo $stocks->stock_id; ?>" style="color: #808080; background:#ffa500; font-weight: bold;">
+              <i class="fa-solid fa-pen-to-square"></i>&nbsp;Düzenle
+            </button>
+          </td>
+          <td id="tableStock_id"><?php echo $stocks->stock_id; ?></td>
+          <td id="tableStock_med" <?php echo $stocks->medicine_id; ?>">
+            <?php
+            foreach ($medicines as $medicine) {
+              if ($medicine->medicine_id == $stocks->medicine_id) {
+                echo $medicine->name;
+                break;
+              }
+            } ?></td>
+          <td id="tableStock_piece"><?php echo $stocks->piece; ?></td>
+          <td id="tableMedCat_<?php echo $stocks->medicine_id; ?>">
+            <?php
+            foreach ($categories as $category) {
+              if ($category->category_id == $stocks->category_id) {
+                echo $category->c_type;
+                break;
+              }
+            } ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 </div>
 
 <?php foreach ($stock as $stocks) : ?> <!-- Düzeltme -->
-    <form id="personelEdit<?php echo $stocks->stock_id; ?>"> <!-- Düzeltme -->
-        <div class="modal fade" id="staticBackdrop<?php echo $stocks->stock_id; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Stok Düzenleme</h5> <!-- Başlık düzeltilmesi -->
-                        <input type="hidden" value="<?php echo $stocks->stock_id; ?>" name="stock_id"> <!-- Doğru değişken -->
-                        <button type="button" class="btn-close" id="kapatBtn<?php echo $stocks->stock_id; ?>" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="input-group mb-3">
-                            <span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
-                            <input name="adi" type="text" class="form-control" placeholder="Güncel stok adedi" aria-label="Stok adedi" value="<?php echo $stocks->piece; ?>">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
-                            <input name="ilac" type="text" class="form-control" placeholder="ilaç adı" aria-label="İlaç Adı" value="<?php echo $stocks->medicine_id; ?>">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-primary" type="submit">Kaydet</button> <!-- Submit button -->
-                    </div>
-                </div>
+  <form id="personelEdit<?php echo $stocks->stock_id; ?>"> <!-- Düzeltme -->
+    <div class="modal fade" id="staticBackdrop<?php echo $stocks->stock_id; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Stok Düzenleme</h5> <!-- Başlık düzeltilmesi -->
+            <input type="hidden" value="<?php echo $stocks->stock_id; ?>" name="stock_id"> <!-- Doğru değişken -->
+            <button type="button" class="btn-close" id="kapatBtn<?php echo $stocks->stock_id; ?>" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="input-group mb-3">
+              <span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
+              <input name="adi" type="text" class="form-control" placeholder="Güncel stok adedi" aria-label="Stok adedi" value="<?php echo $stocks->piece; ?>">
             </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
+              <input name="ilac" type="text" class="form-control" placeholder="ilaç adı" aria-label="İlaç Adı" value="<?php echo $stocks->medicine_id; ?>">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-primary" type="submit">Kaydet</button> <!-- Submit button -->
+          </div>
         </div>
-    </form>
+      </div>
+    </div>
+  </form>
 <?php endforeach; ?>
 </body>
 <script>
@@ -97,7 +111,7 @@
     notifDiv.innerHTML = notificationContent;
   }
 
-  $(document).ready(function () {
+  $(document).ready(function() {
     var forms = document.getElementsByTagName('form');
     var base_url = window.location.origin + "/" + window.location.pathname.split("/")[1];
     for (var i = 0; i < forms.length; i++) {
@@ -108,7 +122,7 @@
         var ajaxDeleteID = '#delete_' + employeeID;
 
         // Silme düğmesi olayı
-        $(ajaxDeleteID).click(function (e) {
+        $(ajaxDeleteID).click(function(e) {
           e.preventDefault();
           var confirmMessage = "Bu stoğu silmek istediğinize emin misiniz?";
           var confirmed = confirm(confirmMessage);
@@ -121,12 +135,14 @@
             $.ajax({
               type: "POST",
               url: _URL,
-              data: { id: id },
-              success: function (result) {
+              data: {
+                id: id
+              },
+              success: function(result) {
                 showSnackbar(null, "Stok Başarıyla Silindi!", 1);
                 document.getElementById("tableID_" + id).remove();
               },
-              error: function () {
+              error: function() {
                 showSnackbar(null, "Stok Silinirken Bir Hata Oluştu!", 0);
               }
             });
@@ -134,7 +150,7 @@
         });
 
         // Form gönderme olayı
-        $(ajaxFormID).on('submit', function (e) {
+        $(ajaxFormID).on('submit', function(e) {
           e.preventDefault();
           var base_url = window.location.origin + "/" + window.location.pathname.split("/")[1];
           var _URL = base_url + "/EmployeeEdit"; // Çalışan güncelleme URL'si
@@ -150,12 +166,12 @@
             type: 'POST',
             url: _URL,
             data: formData,
-            success: function (response) {
+            success: function(response) {
               if (response == "200") {
                 showSnackbar(null, "Çalışan Güncellendi!", 1);
                 document.getElementById(e.delegateTarget[1].id).click();
                 var currentEmpID = e.delegateTarget[1].id.replace("kapatBtn", "");
-                var trElement = document.getElementById("tableID_" + currentEmpID); 
+                var trElement = document.getElementById("tableID_" + currentEmpID);
                 trElement.querySelector("#tableEmp_id").innerHTML = formDataArray["emp_id"];
                 trElement.querySelector("#tableEmp_name").innerHTML = formDataArray["adi"];
                 trElement.querySelector("#tableEmp_surname").innerHTML = formDataArray["soyadi"];
@@ -164,7 +180,7 @@
                 showSnackbar(null, "Çalışan Güncellenirken Bir Hata Oluştu!", 0);
               }
             },
-            error: function () {
+            error: function() {
               console.error("Bir hata oluştu.");
             }
           });
@@ -172,7 +188,6 @@
       }
     }
   });
-
 </script>
 
 
